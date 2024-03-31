10eb478b3653f33a5592e8913e30c98368904759
Sharadindu Das <remodas7774@gmail.com>
2024-03-31 12:22:08 +0530

    undefined not defined

---
+++ 03_undefined_notdefined.js
@@ -0,0 +1,7 @@
+console.log(a) // undefined as it has been allocated some memory during memory allocation phase
+var a = 10 // assigning value to it
+console.log(a) // assigned a value 
+
+console.log(x) // not defined means it has not been allocated any memory during memory allocation phase
+
+a = undefined // never do this as it is a bad practice
